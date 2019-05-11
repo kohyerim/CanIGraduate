5f4ae823d139eea8f37dcc919104825114569a13
고혜림 <kohyerim@gohyelim-ui-MacBookPro.local>
2019-05-11 21:57:14 +0900

    intro fix

--- app/src/main/java/com/example/canigraduate/IntroActivity.java
+++ app/src/main/java/com/example/canigraduate/IntroActivity.java
@@ -1,8 +1,11 @@
 package com.example.canigraduate;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class IntroActivity extends Activity {
 
@@ -10,10 +13,15 @@ public class IntroActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        Drawable drawable = getResources().getDrawable(R.drawable.graduation);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.setImageDrawable(drawable);
+
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
                 finish();
             }
         }, 2000);
