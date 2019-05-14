9235383684b877d7d2d8cf58fd5c53f57db1d212
kohyerim <kohyerim78@gmail.com>
2019-05-14 12:54:44 +0900

    change xml

--- app/src/main/java/com/example/canigraduate/MainActivity.java
+++ app/src/main/java/com/example/canigraduate/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.canigraduate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    protected void onClick(View view){
+        startActivity(new Intent(MainActivity.this, Graduationpoint_kakaoActivity.class));
+        finish();
+    }
 }
